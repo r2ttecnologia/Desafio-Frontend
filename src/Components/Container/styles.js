962c8b5166f75a442e9b3ff05eb21834bfb4ef09
WelderBM <Welderb40@gmail.com>
2022-01-25 16:47:52 -0300

    responsivity ready

--- src/Components/Container/styles.js
+++ src/Components/Container/styles.js
@@ -11,4 +11,14 @@ padding: 0 65px 58px 65px;
 display: flex;
 flex-direction: column;
 align-items: center;
+@media(max-width: 500px) {
+    width: 100%;
+    min-width: 0;
+}
+@media(max-width: 380px) {
+    padding: 0 30px 30px 30px;
+}
+@media(max-width: 300px) {
+    border-radius: 0;
+}
 `
--- src/Components/Img/styles.js
+++ src/Components/Img/styles.js
@@ -2,4 +2,7 @@ import styled from "styled-components"
 
 export const Img = styled.img`
 width: 220px;
+@media(max-width: 300px) {
+    width: 100%;
+}
 `
